8f66ccb558bb4ee40e4c1a2e7fe1916b11dbba2f
Estkhar Ansari <estkharansari123@gmail.com>
2023-09-29 15:01:40 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,58 @@
+const inputBox = document.querySelector(".input-box");
+const searchBtn = document.getElementById("searchBtn");
+const weather_img = document.querySelector(".weather-img");
+const temperature = document.querySelector(".temperature");
+const description = document.querySelector(".description");
+const humudity = document.getElementById("humidity");
+const wind = document.getElementById("wind-speed");
+const location_not_found = document.querySelector(".location-not-found");
+const weather_body = document.querySelector(".weather-body");
+
+async function checkWeather(city) {
+  const api_key = "cfac774077d37c6f4f3d74ef7e53ac17";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+  const weather_data = await fetch(`${url}`).then((response) =>
+    response.json()
+  );
+
+  if(weather_data.cod === "404") {
+    location_not_found.style.display = "flex";
+    weather_body.style.display = "none";
+    console.log("error");
+    return;
+  }
+
+  location_not_found.style.display = "none";
+  weather_body.style.display = "flex";
+  temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+  description.innerHTML = `${weather_data.weather[0].description}`;
+  humudity.innerHTML = `${weather_data.main.humidity}`;
+  wind.innerHTML = `${weather_data.wind.speed}`;
+
+  switch (weather_data.weather[0].main) {
+    case "Few cloud":
+      weather_img.src = "assets/cloud.png";
+      break;
+    case "Cloud":
+      weather_img.src = "assets/cloud.png";
+      break;
+    case "Clear":
+      weather_img.src = "assets/clear.png";
+      break;
+    case "Rain":
+      weather_img.src = "assets/rain.png";
+      break;
+    case "Mist":
+      weather_img.src = "assets/mist.png";
+      break;
+    case "Snow":
+      weather_img.src = "assets/snow.png";
+      break;
+  }
+  // console.log(weather_data);
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(inputBox.value);
+});
